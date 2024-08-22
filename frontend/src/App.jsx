9bd571c6771df94725141e359ed9a8e2dae02303
JAbasith <abdhulbasith071@gmail.com>
2024-08-23 01:07:42 +0530

    correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,26 +6,12 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
-
 import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
 
   const [showLogin,setShowLogin] = useState(false)
   
-  return (
-    <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-      <div className='app'>
-        <Navbar setShowLogin={setShowLogin} />
-
-import LoginPopup  from './components/LoginPopup/LoginPopup'
-
-const App = () => {
-
-  //set initial value as a false 
-  const [showLogin, setShowLogin] = useState(false)
-
   return (
     <>
     {/* if else statement */}
