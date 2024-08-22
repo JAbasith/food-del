b848da469d667856b8c9529c40f790d3303559ee
JAbasith <abdhulbasith071@gmail.com>
2024-08-22 21:28:25 +0530

    login page

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className='header-contents'>
-        <h2>Oredr your favourite food here</h2>
+        <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of diches crafted with finest ingredients and culinary expertise. Our mission is to satisfy your craving and elevate your dining experience, one delicious meal at a time</p>
         <button>View Menu</button>
       </div>
