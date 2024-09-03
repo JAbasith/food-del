728d51f4b3c4955d5f922ecfd79fc6ebaf898b10
JAbasith <abdhulbasith071@gmail.com>
2024-09-03 11:47:50 +0530

    admin remove
    list,remove items

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,25 +1,32 @@
-import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Sidebar from "./components/Sidebar/Sidebar";
-import { Routes,Route } from "react-router-dom";
-import Add from "./pages/Add/Add";
-import List from "./pages/List/List";
-import Orders from "./pages/Orders/Orders";
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const App = () => {
+const App = ({url}) => {
+
+  const url = "http://localhost:4000";
+
   return (
     <div>
-      <Navbar/>
-      <hr/>
+      <ToastContainer />
+      <Navbar />
+      <hr />
       <div className="app-content">
-        <Sidebar/>
+        <Sidebar />
         <Routes>
-          <Route path="/add" element={<Add/>}/>
-          <Route path="/list" element={<List/>}/>
-          <Route path="/orders" element={<Orders/>}/>
-          
+          <Route path="/add" element={<Add url={url}/>} />
+          <Route path="/list" element={<List url={url}/>} />
+          <Route path="/orders" element={<Orders url={url}/>} />
         </Routes>
       </div>
-  </div>);
+    </div>
+  )
 }
-export default App;
+
+export default App
