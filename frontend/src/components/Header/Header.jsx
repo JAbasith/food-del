f5a4b0019a2f36ec5b62a85f2ba153020e15e12f
Praveen Madushan <prvnmadushan@gmail.com>
2024-09-05 12:32:40 +0530

    Changed some styles and logos

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import './Header.css'
 
+
 const Header = () => {
   return (
     <div className='header'>
       <div className="header-contents">
         <h2>
-            Order your favourite food here
-        </h2>
+        Craving Something Delicious? </h2>
+        <h3>Order Your Favorite Food Here!</h3>
         <p>
             Choose from diverse menu featuring a delectable array of dishes crafted with the finest  ingredients and culinary experience, one delicious meal at a time.
         </p>
-        <button>View Menu</button>
+        <a href='#explore-menu'><button>View Menu</button> </a>
+
+        
       </div>
     </div>
   )
